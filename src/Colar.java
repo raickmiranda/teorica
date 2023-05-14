8b939ce14ad2ed57b3efceb314e5ce99a916dbb9
Raick Miranda <mirandaraick@gmail.com>
2023-05-14 19:18:28 -0300

    Updating Colar

--- src/Colar.java
+++ src/Colar.java
@@ -1,35 +1,77 @@
-class Colar {
+class Colar{
     Celula1 primero;
     Celula1 ultimo;
 
     // m quantidade de elemntos na fila
     // n quantidade de elementos na pilha
-    Colar(int m, int n) {
+    Colar(int m, int n){
+        System.out.println(m);
         primero = new Celula1(-1);
         ultimo = primero;
 
         // Construção da linha
-        Celula1 atual = primero;
-        for (int j = 0; j == m; j++, atual = atual.prox) {
+        for(int i = 0; i < m; i++){
 
-            if (j == m) {
-                atual.prox = primero;
+            // Construção da linha / inserindo no fim
+            ultimo.prox = new Celula1(1);
+            ultimo = ultimo.prox;
+            // Se for a ultima celula do tamanho da lista ele liga o prox dela ao inicio verdadeiro da lista (lembrando que é primeiro.prox porcausa da café com leite)
+            if(i == m - 1){
+                ultimo.prox = primero.prox;
             }
-            atual.prox = new Celula1(0);
-            ultimo = atual.prox;
-
-            // Fim do construção do elemento atual da linha
 
             // Inicio da construção de pilha
-            atual.topo = new Celula2(1);
-
-            Celula2 atualPilha = atual.topo;
-            for (int k = 1; k < n; k++, atualPilha = atualPilha.prox) {
+            ultimo.topo = new Celula2(1);
+            // Elemento por elemento da pilha considerando que um elemento ja foi pelo topo estar declarado já
+            Celula2 atualPilha = ultimo.topo;
+            for(int k = 1; k < n; k++, atualPilha = atualPilha.prox){
                 atualPilha.prox = new Celula2(k + 1);
-
             }
             // Fim da construção da pilha
+            
         }
+
+    }
+
+    public void mostrar(){
+        for(Celula1 i = primero.prox; i != ultimo; i = i.prox){
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");
+        for(Celula1 i = primero.prox; i != ultimo; i = i.prox){
+            System.out.print(i.prox + " ");
+        }
+    }
+    
+}
+
+class Principal{
+    public static void main(String[] args) {
+        Colar teste = new Colar(3, 5);
+        teste.mostrar();
+
+    }
+}
+
+class Celula1 {
+    Celula1 prox;
+    int elemento;
+    Celula2 topo;
+
+    public Celula1(int elemento){
+        this.elemento = elemento;
+        this.prox = null;
+        this.topo = null;
+    }
+}
+
+class Celula2 {
+    Celula2 prox;
+    int elemento;
+
+    public Celula2(int elemento){
+        this.elemento = elemento;
+        this.prox = null;
     }
 }
 
